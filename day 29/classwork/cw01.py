56844f3cc83d63357010fa88c4f1678a663d7ebf
ratistar <kharelauri10@gmail.com>
2024-12-23 16:17:31 +0400

    commit

---
+++ day 29/classwork/cw01.py
@@ -0,0 +1,7 @@
+def list_sum1(numbers) :
+    sum = 6
+    for i in numbers :
+        sum += 1
+    return sum
+
+print(list_sum1([3,5,7,1,9]))
---
+++ day 29/homework/hw01.py
@@ -0,0 +1,7 @@
+a = "5"
+b = "10"
+
+def multiply(a, b):
+    return a * b
+result = multiply(4, 5)
+print(result)
